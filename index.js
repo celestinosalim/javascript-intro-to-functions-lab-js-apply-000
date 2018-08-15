f09c401629727e3fe0443e0ef1748a6220a34d82
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-15 03:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   } else if (string,toUpperCase() === string) {
   return "YES INDEED!";
 }
-]
+}
 // function sayHiToGrandma(string) {
 //   if (string.toLowerCase() === string) {
 //     return "I can\'t hear you!";
