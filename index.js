9f839a3d1429649f08188e75bc6bb44fe369a291
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-15 03:03:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
-    return "I love you, too";
-    else if (string.toLowerCase() === string {
-      return "I can\'t hear you!";
-        else return "YES INDEED!";
+
+  if (string.toLowerCase() === string) {
+    return " I can\'t hear you!";
+    else if (string.toUpperCase() === string) {
+      return "YES INDEED!";
+      else ( return "I love you, too.")
     }
   }
 }
