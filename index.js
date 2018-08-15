cfc2fbc210ec37f53f309047fc1868417ad816e9
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-15 03:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ function sayHiToGrandma(string) {
   }
 
   else if (string.toUpperCase() === string) {
-  return "YES INDEED!";
+    return "YES INDEED!";
 }
 
   else {
-  return "I love you, too."
+    return "I love you, too."
 
   };
 }
